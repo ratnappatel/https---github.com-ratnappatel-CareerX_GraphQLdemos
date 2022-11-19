bf2297dfce61a7743979784bd08b8ad6547730a8
ratnappatel <ratnappatel@gmail.com>
2022-11-19 11:15:34 +0530

    graphql demos

---
+++ todo-mongo-graphql-server-master/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var Schema = require('./schema')
+const { graphqlHTTP }  = require('express-graphql')
+
+var app = express()
+app.use('/', graphqlHTTP.graphqlHTTP({
+  schema: Schema,
+  pretty: true,
+  graphiql: true
+}),);
+app.listen(process.env.PORT || 8080, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(`GraphQL Server is now running on localhost:${process.env.PORT || 8080}`)
+})
